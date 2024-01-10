514b6cce72c690f23e84a592d2d310ad0389b177
Henrique Albuquerque <henrique.albuquerque.248@gmail.com>
2024-01-10 08:01:25 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { ChromePicker } from 'react-color'
 import { io } from 'socket.io-client'
 import { DrawLine } from '../utils/drawLine'
 
-const socket = io('http://localhost:3001')
+const socket = io('https://teste-3-server.onrender.com:3001')
 
 
 interface pageProps{}
